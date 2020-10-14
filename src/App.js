69c2e4df92bbb8728acca2ac274eca781914bb75
isiahZaye <60477001+isiahZaye@users.noreply.github.com>
2020-10-13 21:13:11 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Fire tetris Game with React, using Redux, and SVG!!!! </h1>
+    </div>
+  );
+}
+
+export default App;
